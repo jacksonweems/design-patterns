043654ecfc70f3f8d3172ecc849024518659df7f
Jackson <97468261+jacksonweems@users.noreply.github.com>
2022-09-24 14:13:29 -0400

    Update AssignmentIterator.java

--- iterator/AssignmentIterator.java
+++ iterator/AssignmentIterator.java
@@ -5,26 +5,26 @@ import java.util.Iterator;
 public class AssignmentIterator implements Iterator 
 {
     private Assignment[] assignments;
-	private int position = 0;
+    private int position = 0;
 	/**
      * Represents the AssignmentIterator in code
      */
-	public AssignmentIterator(Assignment[] assignments) 
+    public AssignmentIterator(Assignment[] assignments) 
     {
-		this.assignments = assignments;
+    	this.assignments = assignments;
     }
 	/**
      * Goes to next assignmment
      */
     public Assignment next() 
     {
-		Assignment assignmentS = assignments[position];
-		position = position + 1;
-		return assignmentS;
-	}
+	Assignment assignmentS = assignments[position];
+	position = position + 1;
+	return assignmentS;
+    }
 	
-	public boolean hasNext() 
+    public boolean hasNext() 
     {
-		return position<=assignments.length&&assignments[position]!=null;
-	}
+	return position<=assignments.length&&assignments[position]!=null;
+    }
 }
